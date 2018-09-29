1448d79b57324d9fc15d59b57dd7a47ceb986cf2
jerio <houjie20@gmail.com>
2018-09-29 11:05:34 +0800

    eureka服务端

---
+++ eureka-client/src/main/java/cn/jerio/eureka/client/TestController.java
@@ -0,0 +1,23 @@
+package cn.jerio.eureka.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Jerio on 2018/09/29
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+}
